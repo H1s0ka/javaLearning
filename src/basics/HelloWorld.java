33ad6e4e7cf46ca7621ba57fb6f385b450831c40
H1s0ka <36103781+H1s0ka@users.noreply.github.com>
2018-02-03 15:46:06 +0200

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World !!");
 		System.out.println("This is an addition to check git");
+		System.out.println("This is an addition through github");
 	}
 }
